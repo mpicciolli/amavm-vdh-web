7e7be3b28f74e507ab65a5ed76762501a1e8c040
Julien Blin <julien.blin@gmail.com>
2018-11-04 11:31:48 -0500

    Consolidated bicycle paths segments.

--- refresh-bicycle-paths.js
+++ refresh-bicycle-paths.js
@@ -13,11 +13,37 @@ const downloadBicyclePaths = () => {
       str += chunk;
     });
     response.on('end', function () {
-      const bpData = JSON.parse(str);
-      const filteredData = bpData.items.map((x) => ({
+      const bpData = JSON.parse(str).items;
+
+      const indexedPaths = bpData.reduce((acc, cur) => { acc[cur.id] = cur; return acc;}, {});
+      const ids = Object.keys(indexedPaths);
+      for (let index = 0; index < ids.length; index++) {
+        const id = ids[index];
+        const segment = indexedPaths[id];
+        if (segment) {
+          const lastPoint = segment.geometry.coordinates[0][segment.geometry.coordinates[0].length - 1];
+          const segmentWithStartingFirstPoint = Object.values(indexedPaths)
+            .filter((x) =>
+            !!x &&
+            x.network === segment.network &&
+            x.geometry.coordinates[0][0][0] === lastPoint[0] &&
+            x.geometry.coordinates[0][0][1] === lastPoint[1])[0];
+          if (segmentWithStartingFirstPoint) {
+            segmentWithStartingFirstPoint.geometry.coordinates[0]
+            segmentWithStartingFirstPoint.geometry.coordinates[0] = [
+              ...segment.geometry.coordinates[0],
+              ...segmentWithStartingFirstPoint.geometry.coordinates[0]
+            ];
+            indexedPaths[segment.id] = undefined;
+          }
+        }
+      }
+
+      const filteredData = Object.values(indexedPaths).filter((x) => !!x).map((x) => ({
         geometry: x.geometry,
         network: x.network,
       }));
+
       fs.writeFileSync(BICYCLE_PATHS_DATA, JSON.stringify(filteredData), { flags: 'w' });
     });
   });
