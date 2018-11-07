de53b2c432cf0de81a1bea937cb289e9af19e777
Julien Blin <julien.blin@gmail.com>
2018-11-06 21:15:21 -0500

    Implemented permanent observations links. (#2)

--- src/entities.ts
+++ src/entities.ts
@@ -66,3 +66,57 @@ export interface BicyclePath {
   /** The type of bicycle path */
   type: BicyclePathType;
 }
+
+/** Status for an observation. */
+export enum ObservationStatus {
+  Ok = "ok",
+  Ko = "ko",
+}
+
+export enum ObservationAttributes {
+  Snow = "snow",
+  Ice = "ice",
+}
+
+/** Base definition for observations. */
+export interface ObservationBase {
+  /** Attributes to further characterize the observation. */
+  attributes?: ObservationAttributes[];
+  /** Free-form comments. */
+  comment?: string;
+  /** A device identifier (the reporting device). */
+  deviceId: string;
+  /**
+   * A GeoJSON position for the observation.
+   * Be careful - GeoJson Positions are [long, lat, elevation]
+   */
+  position: Position;
+  /** A timestamp of when the observation was done. Unix Epoch in seconds. */
+  timestamp: number;
+}
+
+/** A reported observation */
+export interface ReportedObservation extends ObservationBase {
+  /** Unique id */
+  id: string;
+  /** Associated assets. */
+  assets?: ReportedObservationAsset[];
+}
+
+export enum AssetContentType {
+  Jpeg = "image/jpeg",
+  Png = "image/png",
+}
+
+export interface ObservationAssetBase {
+  /**
+   * The asset content-type
+   */
+  contentType: AssetContentType;
+}
+
+/** An asset attached alongside a ReportedObservation. */
+export interface ReportedObservationAsset extends ObservationAssetBase {
+  /** Base-64 encoded data. */
+  url: string;
+}
